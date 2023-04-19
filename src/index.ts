a1c279d1b6516df6745f3e5b7790026c53163bae
Mark Katerberg <katerberg@fastmail.fm>
2023-04-18 23:20:43 -0500

    View of filter list

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,7 @@ function sortingFunction(a: SetOfCards, b: SetOfCards): -1 | 0 | 1 {
   } else if (aLength < bLength) {
     return 1;
   }
-  // return 0;
   return a.releaseDate < b.releaseDate ? 1 : -1;
-  // return 0;
 }
 
 function populateRollUp(data: SetsForCardsResponseData[]): void {
@@ -49,7 +47,7 @@ function populateRollUp(data: SetsForCardsResponseData[]): void {
           <summary>${set.code} - ${set.displayName}</summary>
           <ul class="card-list">${Object.keys(set.cards)
             .sort((a, b) => (a < b ? -1 : 1))
-            .map((card) => `<li>${card}</li>`)
+            .map((card) => `<li class="list-card">${card}</li>`)
             .join('')}</ul>
         </details>
         `,
@@ -79,6 +77,9 @@ function handleSetsForCardsResponse(result: {data: SetsForCardsResponseData[]; e
 async function submitListener(e: Event): Promise<void> {
   e.preventDefault();
 
+  const filtersSection = document.getElementById('filters');
+  filtersSection?.classList.remove('hidden');
+
   const cardInput = document.getElementById('card-input') as HTMLInputElement;
   const validationMessage = document.getElementById('validation-message');
   if (!cardInput || cardInput.value === '') {
@@ -99,8 +100,46 @@ async function submitListener(e: Event): Promise<void> {
   }
 }
 
+function populatePulledCards(): void {
+  const noFiltersAppliedSection = document.getElementById('no-filters-applied');
+  const pulledCards = document.getElementById('pulled-cards');
+  const pulledCardsSection = document.getElementById('pulled-cards-section');
+  if (!pulledCards || !pulledCardsSection || !noFiltersAppliedSection) {
+    return;
+  }
+  if (globalThis.pulledCards.length) {
+    pulledCardsSection.classList.remove('hidden');
+    noFiltersAppliedSection.classList.add('hidden');
+  } else {
+    pulledCardsSection.classList.add('hidden');
+    noFiltersAppliedSection.classList.remove('hidden');
+  }
+  pulledCards.innerHTML = globalThis.pulledCards.map((card) => `<div>${card}</div>`).join('');
+}
+
+function clearPulledCardsListener(e: Event): void {
+  e.preventDefault();
+  globalThis.pulledCards = [];
+  populatePulledCards();
+}
+
+function rollUpClickListener(e: Event): void {
+  const target = e.target as HTMLElement;
+  if (target && target.matches('li.list-card') && target.textContent) {
+    if (!globalThis.pulledCards.includes(target.textContent)) {
+      globalThis.pulledCards.push(target.textContent);
+      populatePulledCards();
+    }
+  }
+}
+
 window.addEventListener('load', () => {
+  globalThis.pulledCards = [];
   const cardSearchForm = document.getElementById('card-search-form');
+  const clearPulledCardsButton = document.getElementById('clear-pulled-cards');
+  const rollUp = document.getElementById('card-search-roll-up');
 
   cardSearchForm?.addEventListener('submit', submitListener);
+  clearPulledCardsButton?.addEventListener('click', clearPulledCardsListener);
+  rollUp?.addEventListener('click', rollUpClickListener);
 });
