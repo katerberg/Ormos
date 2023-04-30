8de79ab9e58fd6fbb141dab45fa25c3c696664d6
Mark Katerberg <katerberg@fastmail.fm>
2023-04-29 23:31:30 -0500

    Fixes typo

--- deploy-cloud.sh
+++ deploy-cloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cp /etc/letsencrypt/live/api.stlotus.org/* ./creds/
-curl https://mtgjson.com/api/v5/AllPrintings.sql > data/AllPrintings.sqlite
+curl https://mtgjson.com/api/v5/AllPrintings.sqlite > data/AllPrintings.sqlite
 docker-compose build
 docker-compose up -d
